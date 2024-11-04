f08ef167575887168caa3a43150bf393e5bdd0e6
LoftSpace <yongjuni30@gmail.com>
2024-11-04 19:17:58 +0900

    feat: draw main page

--- frontend/src/component/views/Main/Main.js
+++ frontend/src/component/views/Main/Main.js
@@ -1,23 +1,69 @@
 import  React from 'react'
+import  { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Main.css';
 
 function Main() {
+  const navigate = useNavigate();
+  const navigateToThreadPage = () => {
+    navigate("/MyThreads");
+  };
+  const [Host, setHost] = useState("");
+  const [Port, setPort] = useState("");
+  const [TotalThreadCount, setTotalThreadCount] = useState("");
     return (
-        <div>
-              <p>
-          이 화면에 첫 페이지 넣어주세요 아래 링크는 우리 쓰레드 보여줄  페이지
+        <div className = "container">
+        <p className="title">
+          Monitor your Thread pool
         </p>
+        
+          <div className="form-group">
+            <label className = "label" htmlFor="hostInput">Enter your Host</label>
+            <input className = "input"
+              id = "hostInput"
+              type = "text"
+              value = {Host}
+              onChange = {(e) => {
+                console.log(e);
+                setHost(e.target.value);
+                
+              }} />
+            </div>
+        <br></br>
 
-        <a
-          className="App-link"
-          href="./myThreads"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go to see my threads!
-        </a>
-      
+          <div className="form-group">
+            <label className = "label" htmlFor="hostInput">Enter your Port</label>
+            <input className = "input"
+              id = "PortInput"
+              type = "text"
+              value = {Port}
+              
+              onChange = {(e) => {
+                console.log(e);
+                setPort(e.target.value);
+              }} />
+          </div>
+        <br></br>
 
-        </div>
+          <div className="form-group">
+            <label className = "label" htmlFor="hostInput">Enter your thread number</label>
+            <input className = "input"
+              id = "HostInput"
+              type = "text"
+              value = {TotalThreadCount}
+              
+              onChange = {(e) => {
+                console.log(e);
+                setTotalThreadCount(e.target.value);
+              }} />
+          </div>
+        <br></br>
+          <button className="button" size = "large" onClick = {navigateToThreadPage}>
+            see your threads!
+          </button>
+       </div>
     )
 }
+
+
 export default Main
